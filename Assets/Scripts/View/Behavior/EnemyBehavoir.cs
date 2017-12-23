c4393ea0b0e7848337e0d7907f7d27b59cab29c4
4419-3-3\user <byebye90321@gmail.com>
2017-12-23 10:00:24 +0800

    1.

---
+++ Assets/Scripts/View/Behavior/EnemyBehavoir.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavoir : MonoBehaviour {
+
+    private Animator animator;
+
+    private void Awake() {
+        animator.SetTrigger("die");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
